ae5468e55b41d6a6dc0dda5745e631d3b31e0cb3
Mridul Kapoor <66908728+mridul8920@users.noreply.github.com>
2022-06-15 22:05:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,9 +40,35 @@ GetPuzzle.onclick = function () {
 }
 
 SolvePuzzle.onclick = () => {
-	SudokuSolver(board, 0, 0, 9);
+	solveSudoku(board);
 };
 
-function SudokuSolver(board, i, j, n) {
-	// Write your Code here
+	function solveSudokuHelper(board, sr, sc) {
+    console.log("1234")
+    if (sr == 9){
+        changeBoard(board);
+        return;
+    }
+
+    if (sc == 9){
+        solveSudokuHelper(board, sr+1, 0)
+        return;
+    }
+
+    if (board[sr][sc] != 0){
+        solveSudokuHelper(board, sr, sc+1);
+        return;
+    }
+
+    for (var i=1; i<=9; i++){
+        if (isSafe(board, sr, sc, i)){
+            board[sr][sc] = i;
+            solveSudokuHelper(board, sr, sc+1);
+            board[sr][sc] = 0;
+        }
+    }
+}
+
+function solveSudoku(board) {
+    solveSudokuHelper(board, 0, 0)
 }
